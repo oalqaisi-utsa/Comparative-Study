4fdf21c194f3faeece8c8f2ac6541ae251f19ee5
Osama Alqaisi <osamah.alqaisi@my.utsa.edu>
2023-06-06 10:17:48 -0500

    Edit Sender

--- Send/Sender.py
+++ Send/Sender.py
@@ -7,6 +7,7 @@ PORT = 8886
 send = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # AF_INET = IP, SOCK_STREAM = TCP
 send.connect(( HOST , PORT ))
 send.settimeout(1800)
+print("Connect")
 
 
 from os import path
@@ -14,33 +15,28 @@ path = path.dirname(__file__) + '\\Yolo\\'
 
 image = None
 total_time = 0
-print("start")
-with open( (path+'CarSend.txt') ,'w')as file:
-    for i in range (1,5):
-        for j in range (5, 1005) :
+for i in range (1,5):
+    for j in range (1, 1001) :
 
-                start_time = cv.getTickCount()
+            start_time = cv.getTickCount()
 
-                image = str(j) + ".jpg"
-                print(f"image name: {i} - {image}")
-                img = cv.imread(path + image)
-                
-                img_bytes = cv.imencode('.jpg',img)[1].tobytes()
-                data =  img_bytes
-                send.sendall(data)
-                end_marker = b'##END##' + image.encode()
-                send.sendall(end_marker)
-                
-                rec = send.recv(512)
-                
-                end_time = (cv.getTickCount()-start_time)/cv.getTickFrequency()
+            image = str(j) + ".jpg"
+            img = cv.imread(path + image)
+            
+            img_bytes = cv.imencode('.jpg',img)[1].tobytes()
+            data =  img_bytes
+            send.sendall(data)
+            end_marker = b'##END##' + image.encode()
+            send.sendall(end_marker)
+            
+            rec = send.recv(512)
+            
+            end_time = (cv.getTickCount()-start_time)/cv.getTickFrequency()
 
-                total_time = total_time + end_time 
-                #file.write(f"{i} - {j} - Sent: {image} - {end_time:.4f} \n" )
+            total_time = total_time + end_time 
+            print(f"{i} - {j} - Sent: {image} - {end_time:.4f} \n" )
 
-                time.sleep(1)
+            time.sleep(1)
 
-#with open('FaceSend.txt','a')as file:
-#    file.write(f"Total Time is {total_time:.4f}" )
 
 send.close()
